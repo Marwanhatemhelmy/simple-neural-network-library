53b596909f8e37b4d02d8cd2cd0cf74c1634f1ca
Marwan Hatem <merohelmy93@gmail.com>
2025-06-03 12:08:26 +0300

    Initial commit

---
+++ SimpleNeuralNetworkLibraryProject/model/Model.java
@@ -0,0 +1,37 @@
+package model;
+
+import dense.Layer;
+import tensor.Tensor;
+
+// only supports two trainable parameters (weights & biases)
+public class Model {
+
+    Tensor[] parametersList;
+    int numberOfTrainableParameters = 2;
+
+    public void saveLayers(Layer[] layers){
+        this.parametersList = new Tensor[layers.length*2];
+        int index = 0;
+        for (int i=0;i<layers.length;i++){
+            for (int j=0;j<numberOfTrainableParameters;j++){
+                if (j==0){
+                    parametersList[index] = layers[i].weights;
+                }else{
+                    parametersList[index] = layers[i].biases;
+                }
+                index++;
+            }
+        }
+    }
+
+    public Tensor[] parameters(){
+        return this.parametersList;
+    }
+
+    public void zeroGrad(){
+        for (int i=0;i<this.parametersList.length;i++){
+            parametersList[i].zeroGrad_();
+        }
+    }
+    
+}
